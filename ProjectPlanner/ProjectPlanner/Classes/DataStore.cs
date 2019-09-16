f063f767ae3edb5548d15e7f882de25ac7c54846
Bobby Belter <b900cool@gmail.com>
2019-09-15 19:04:23 -0700

    Interface Code
    Added UI class and login code

---
+++ ProjectPlanner/ProjectPlanner/Classes/DataStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProjectPlanner.Classes
+{
+    class DataStore
+    {
+        private Hashtable dataBase;
+
+        public DataStore()
+        {
+            SetDataBase(new Hashtable());
+        }
+
+
+
+
+
+
+
+        public Hashtable GetDataBase()
+        {
+            return dataBase;
+        }
+
+        public void SetDataBase(Hashtable value)
+        {
+            dataBase = value;
+        }
+    }
+}
---
+++ ProjectPlanner/ProjectPlanner/Classes/UI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPlanner.Classes
+{
+    class UI
+    {
+        private DataStore dataStore;
+
+
+        public void ListUsers()
+        {
+            foreach(object k in dataStore.GetDataBase().Values)
+            {
+                Console.WriteLine(k.ToString());
+            }
+        }
+
+        //Logs a user in to the system
+        public void LogIn()
+        {
+            Console.Clear();
+            bool loginComplete = false;
+            do //loops while user attempts to login
+            {
+                
+                Console.WriteLine("Enter your login username");
+                string user = Console.ReadLine();
+                if (dataStore.GetDataBase().ContainsKey(user))
+                {
+                    Console.WriteLine("Enter your login password");
+                    string pass = Console.ReadLine();
+                    try //make sure our cast works, the value of the hashed username may not be a User class
+                    {
+                        User userClass = (User)dataStore.GetDataBase()[user];
+                        if (pass.Equals(userClass))
+                        {
+                            loginComplete = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Password entered incorrectly");
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Error getting user class from database");
+                        break;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Username entered incorrectly/username does not exist");
+                }
+                Console.WriteLine("Press 'q' to quit login or any other key to try again");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.KeyChar.Equals('q'))
+                {
+                    return;
+                }
+                Console.Write("\n\n");
+            } while (!loginComplete);
+        }
+    }
+}
