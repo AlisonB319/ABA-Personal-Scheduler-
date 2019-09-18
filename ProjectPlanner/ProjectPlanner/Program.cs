821beb642e1deff23094c18d11b8e50dc46a9290
Alison Burgess <alison.burgess@wsu.edu>
2019-09-18 12:48:05 -0700

    Added namespace declaration in header so that classes can be called in project.cs

--- ProjectPlanner/ProjectPlanner/Program.cs
+++ ProjectPlanner/ProjectPlanner/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectPlanner.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace ProjectPlanner
     {
         static void Main(string[] args)
         {
+            UI newUI = new UI();
+            newUI.LogIn();
         }
     }
 }
