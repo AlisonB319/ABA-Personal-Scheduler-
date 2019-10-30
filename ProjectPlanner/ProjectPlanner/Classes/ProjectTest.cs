a6e04c92cac7ca27b2189d266ab70fe88819759a
itstheholyginger <amariah.delmar@wsu.edu>
2019-10-29 20:56:55 -0700

    init commit for Project tests

---
+++ ProjectPlanner/ProjectPlanner/Classes/ProjectTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using System.Collections;
+using System.Xml.Linq;
+
+namespace ProjectPlanner.Classes
+{
+    [TestFixture]
+    class ProjectTest
+    {
+        [Test]
+        public void TestAddSchedule() {
+            Project project = new Project();
+            var mocked = new Mock<Schedule>();
+        }
+
+    }
+}
