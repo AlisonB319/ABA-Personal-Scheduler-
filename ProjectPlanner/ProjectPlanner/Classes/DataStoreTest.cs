5f1196f3f2afc46fa19c2c530b3d96d6418e0d8f
Alison Burgess <alison.burgess@wsu.edu>
2019-12-04 23:23:21 -0800

    mocking for DataStore
    had to change some functions to virtual so that moq could mock the functions

--- ProjectPlanner/ProjectPlanner/Classes/DataStoreTest.cs
+++ ProjectPlanner/ProjectPlanner/Classes/DataStoreTest.cs
@@ -18,59 +18,33 @@ namespace ProjectPlanner.Classes
         [Test]
         public void TestAuthenticateUsername()
         {
-            DataStore dataStore = new DataStore();
             Hashtable hashtable = new Hashtable();
-            
-            dataStore.SetDataBase(hashtable);
 
-            //check that our database is in fact empty to start
-            Assert.That(dataStore.AuthenticateUsername("test2"), Is.EqualTo(false));
+            // mock the user object to simulate what would in the actual code
+            var user = new Mock<User>();
+            var dataStore = new DataStore();
 
-            User user = new User();
-            hashtable.Add("test", user);
+            user.Setup(u => u.Getemail()).Returns("fname");
+            dataStore.AddData(user.Object.Getemail(), user.Object);
 
-            //check that we can successfully see a user in the hashtable
-            Assert.That(dataStore.AuthenticateUsername("test"), Is.EqualTo(true));
-
-            //check that we cannot see users not in the hashtable
-            Assert.That(dataStore.AuthenticateUsername("test2"), Is.EqualTo(false));
-
-
+            Assert.IsTrue(dataStore.AuthenticateUsername("fname"));
+            Assert.IsFalse(dataStore.AuthenticateUsername("fakeout"));
         }
 
         [Test]
         public void TestAuthenticatePassword()
         {
-            DataStore NewDataStore = new DataStore();
             Hashtable hashtable = new Hashtable();
 
+            var user = new Mock<User>();
+            var dataStore = new DataStore();
+            
+            user.Setup(u => u.AuthenticatePassword("passwd")).Returns(true);
+            dataStore.AddData("username", user.Object);
 
-            /*
-             * This is how mocking should work if the mocking of concrete classes was supported by 
-             * The mocking frameworks for C#
-            */
-            //var user = new Mock<User>();
-            //var dataStore = new Mock<DataStore>(NewDataStore);
-
-            //user.Setup(m => m.AuthenticatePassword("test")).Returns(true);
-            //dataStore.Setup(foo => foo.getUserFromDatabase(It.IsAny<String>())).Returns(user);
-            User NewUser = new User();
-            NewUser.CreateUser("test", "user", "thisisnotanemail@email.com", "test");
-
-
-            hashtable.Add("test", NewUser);
-            NewDataStore.SetDataBase(hashtable);
-
-            //Check that a user that is in the database connot be incorrectly authenticated
-            Assert.That(NewDataStore.AuthenticatePassword("test", "test"), Is.EqualTo(true));
-            //check that a user in the database can be successfully authenticated
-            Assert.That(NewDataStore.AuthenticatePassword("test", "test"), Is.EqualTo(true));
-            //Check that a user not in the database will not be authenticated
-            Assert.That(NewDataStore.AuthenticatePassword("test2", "test"), Is.EqualTo(false));
-
-            //In mocking we could use this to verify that the user classes AuthenticatePassword was calle dthe appropriate amount of times.
-            //user.Verify(mock => mock.AuthenticatePassword("test"), Times.Tree());
+            // make sure to authenticate the username correctly
+            Assert.IsTrue(dataStore.AuthenticatePassword("username", "passwd"));
+            Assert.IsFalse(dataStore.AuthenticatePassword("username", "fakepass"));
         }
-
     }
 }
